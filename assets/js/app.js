cb9fd22c70fcb69d38a9525d05c4cb989163860f
Aliraza <alir1134@gmail.com>
2026-06-23 17:05:53 +0500

    add some new code on home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
 // document.getElementById('navicon').onclick = function() {
 //     this.classList.toggle('open');
 // }
+
+document.querySelectorAll('.dropdown').forEach(function (dropdown) {
+    dropdown.addEventListener('mouseenter', function () {
+        bootstrap.Dropdown.getOrCreateInstance(
+            this.querySelector('.dropdown-toggle')
+        ).show();
+    });
+    dropdown.addEventListener('mouseleave', function () {
+        bootstrap.Dropdown.getOrCreateInstance(
+            this.querySelector('.dropdown-toggle')
+        ).hide();
+    });
+});
