1bf46f4eae0885441ab6cdc3ff6f4762f4907b07
Aliraza <alir1134@gmail.com>
2026-07-15 15:55:11 +0500

    apply new changes on home page

--- resources/views/partials/modals.blade.php
+++ resources/views/partials/modals.blade.php
@@ -14,6 +14,9 @@
 		<div class="modal fade" id="galleryModal">
 			<div class="modal-dialog modal-lg modal-dialog-centered">
 				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
 					<div class="modal-body">
 						<div class="swiper popupSlider">
 							<div class="swiper-wrapper"></div>
