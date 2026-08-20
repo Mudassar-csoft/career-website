17324a8f7ec727bb0b85501203677f5692d60656
Mudassar-csoft <mudassar@csoftsystems.com>
2026-08-20 18:58:58 +0500

    updates

--- config/campus_videos.php
+++ config/campus_videos.php
@@ -3,17 +3,17 @@
 return [
     [
         'name' => 'Jinnah Colony Branch',
-        'thumbnail' => 'assets/images/img04.png',
+        'thumbnail' => 'assets/images/campuses/jinnah-colony-branch.jpeg',
         'video_url' => 'https://youtu.be/uLFQt8fIC6c?si=U-voH36k8yxzMfna',
     ],
     [
         'name' => 'Millat Chowk Branch',
-        'thumbnail' => 'assets/images/img04.png',
+        'thumbnail' => 'assets/images/campuses/millat-chowk-branch.jpeg',
         'video_url' => 'https://youtu.be/W3hfkCW1U-w?si=z4THrzEAXcllG769',
     ],
     [
         'name' => 'Satyana Road Branch',
-        'thumbnail' => 'assets/images/img04.png',
+        'thumbnail' => 'assets/images/campuses/satyana-road-branch.jpeg',
         'video_url' => 'https://youtu.be/0Zdy-n4z6LU?si=PXOmlPeCLuLak-M2',
     ],
     [
