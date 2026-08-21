abf3668f6df47f63fd02b1b70d66b6e0f7437a33
Mudassar-csoft <mudassar@csoftsystems.com>
2026-08-21 14:51:22 +0500

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,19 @@ class HomeController extends Controller
     public function index()
     {
         $featuredCourses = Course::with(['category', 'mode'])->where('is_featured', true)->latest()->take(6)->get();
+        $eventsWidget = Event::where('event_date', '>=', now()->toDateString())
+            ->orderBy('event_date')
+            ->take(4)
+            ->get();
+
+        if ($eventsWidget->count() < 4) {
+            $eventsWidget = $eventsWidget->concat(
+                Event::where('event_date', '<', now()->toDateString())
+                    ->orderByDesc('event_date')
+                    ->take(4 - $eventsWidget->count())
+                    ->get()
+            )->values();
+        }
 
         if ($featuredCourses->isEmpty()) {
             $featuredCourses = Course::with(['category', 'mode'])->latest()->take(6)->get();
@@ -20,7 +33,7 @@ class HomeController extends Controller
         return view('pages.home', [
             'blogs' => Blog::latest()->take(4)->get(),
             'newsWidget' => News::with('type')->latest()->take(4)->get(),
-            'eventsWidget' => Event::where('event_date', '>=', now()->toDateString())->orderBy('event_date')->take(4)->get(),
+            'eventsWidget' => $eventsWidget,
             'featuredCourses' => $featuredCourses,
         ]);
     }
