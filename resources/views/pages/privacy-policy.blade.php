44c23c6ea1e4385829abd05d3b3f82dea1c42704
Aliraza <alir1134@gmail.com>
2026-08-25 20:02:37 +0500

    create privacy-policy page and style

--- resources/views/pages/privacy-policy.blade.php
+++ resources/views/pages/privacy-policy.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Privacy Policy | Career Website')
-@section('body_class', 'contact-page')
+@section('body_class', 'policy-page')
 @section('content')
 <section class="top-banner">
     <div class="container">
@@ -20,7 +20,7 @@
 <section class="pp-text">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-7">
                 <span>Effective june 7, 2012</span>
                 <h4>
                     Our Privacy Policy has been updated
@@ -29,20 +29,18 @@
                     Your Privacy Matters
                 </h3>
                 <p>
-                    orem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the
-                    dummy text of the printing and orem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the
-                    dummy text of the printing and
+                    At Career Institute, we are committed to protecting your privacy and
+                    ensuring the security of your personal information. We collect and
+                    process data responsibly to provide educational services, improve
+                    user experience, and support student success.
                 </p>
                 <p>
-                    orem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the
-                    dummy text of the printing and orem Ipsum is simply dummy text of 
-                    typesetting industry. Lorem Ipsum has been the
-                    dummy text of the printing and
+                    Information provided through admissions, inquiries, registrations,
+                    and website interactions may be used to communicate with you,
+                    process applications, and deliver relevant services.
+                    We implement appropriate security measures to safeguard
+                    your data and maintain its confidentiality.
                 </p>
-                <a href="">Watch our Privacy Policy Video</a>
             </div>
         </div>
     </div>
