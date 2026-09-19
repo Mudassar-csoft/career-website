26ebd01a167e829f2e63c1bb4be8e55220c4097c
Aliraza <alir1134@gmail.com>
2026-09-19 17:25:36 +0500

    update new image

--- resources/views/pages/stories.blade.php
+++ resources/views/pages/stories.blade.php
@@ -100,7 +100,7 @@
             </div>
             <div class="col-lg-12">
                 <div class="img-hold">
-                    <img src="{{ asset('assets/images/img102.png') }}" alt="Career Institute student success story">
+                    <img src="{{ asset('assets/images/img107.png') }}" alt="Career Institute student success story">
                 </div>
             </div>
         </div>
